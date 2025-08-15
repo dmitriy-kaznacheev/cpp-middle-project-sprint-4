bb20697ba7eb18472d6822350ca4ae9470869fb8
Dmitriy Kaznacheev <dk@example.com>
2025-08-15 14:56:03 +0500

    [fix] memory optimization

--- src/metric_impl/cyclomatic_complexity.cpp
+++ src/metric_impl/cyclomatic_complexity.cpp
@@ -30,16 +30,15 @@ CyclomaticComplexityMetric::CalculateImpl(const function::Function &f) const {
       "except_clause"sv,   "finally_clause"sv,   "match_statement"sv,
       "case_pattern"sv,    "assert_statement"sv, "conditional_expression"sv};
 
-  auto res = f.ast | std::views::split('(') | std::views::drop(1) |
-             std::views::transform([](auto &&r) {
-               auto sv = std::string_view{r};
-               return sv.substr(0, sv.find_first_of(" :"));
-             }) |
-             std::views::filter(
-                 [](auto &&sv) { return keys.contains(std::string{sv}); }) |
-             std::ranges::to<std::vector>();
-
-  return static_cast<int>(res.size());
+  auto res = std::ranges::distance(
+      f.ast | std::views::split('(') | std::views::drop(1) |
+      std::views::transform([](auto &&r) {
+        auto sv = std::string_view{r};
+        return sv.substr(0, sv.find_first_of(" :"));
+      }) |
+      std::views::filter(
+          [](auto &&sv) { return keys.contains(std::string{sv}); }));
+  return static_cast<int>(res);
 }
 
 std::string CyclomaticComplexityMetric::Name() const {
--- src/metric_impl/parameters_count.cpp
+++ src/metric_impl/parameters_count.cpp
@@ -20,14 +20,13 @@ using namespace std::string_literals;
 
 MetricResult::ValueType
 CountParametersMetric::CalculateImpl(const function::Function &f) const {
-  auto res =
+  auto res = std::ranges::distance(
       f.ast | std::views::split('\n') |
       std::views::transform([](auto &&r) { return std::string_view{r}; }) |
       std::views::take_while([](auto &&sv) { return !sv.contains("body"); }) |
-      std::views::filter([](auto &&sv) { return sv.contains("identifier"); }) |
-      std::ranges::to<std::vector>();
+      std::views::filter([](auto &&sv) { return sv.contains("identifier"); }));
 
-  return static_cast<int>(res.size() - 1);
+  return static_cast<int>(res - 1);
 }
 
 std::string CountParametersMetric::Name() const { return "parameters_count"s; }
